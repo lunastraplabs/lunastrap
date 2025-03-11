f13eb6fec4bf8a412fda54d2b0142fcf7a1a6f5c
lunastrap <uraniusrevive@gmail.com>
2025-03-10 18:53:21 -0700

    error fix

--- build.cmd
+++ build.cmd
@@ -2,10 +2,10 @@
 setlocal enabledelayedexpansion
 echo [INFO] Starting Lunastrap publishing...
 echo [INFO] Running dotnet publish...
-dotnet publish Lunastrap /p:PublishProfile=Publish-x64 >nul 2>&1
+dotnet publish Bloxstrap /p:PublishProfile=Publish-x64 >nul 2>&1
 if %errorlevel% neq 0 (
     echo [ERROR] Publishing failed! Check errors below.
-    dotnet publish Lunastrap /p:PublishProfile=Publish-x64 2>&1
+    dotnet publish Bloxstrap /p:PublishProfile=Publish-x64 2>&1
     echo [ERROR] Publishing failed!
     exit /b %errorlevel%
 )
